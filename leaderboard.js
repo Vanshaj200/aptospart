429d2064790c72aadaa5c17bd1656ebbdd0adf0e
Vanshaj200 <140325736+Vanshaj200@users.noreply.github.com>
2024-10-26 14:05:43 +0530

    Delete leaderboard.js

--- leaderboard.js
+++
@@ -1,9 +0,0 @@
-fetch('/leaderboard')
-    .then(response => response.json())
-    .then(data => {
-        const leaderboard = document.getElementById('leaderboard');
-        data.forEach((entry, index) => {
-            leaderboard.innerHTML += `
-                <p>${index + 1}. ${entry.username} - ${entry.score} points</p>`;
-        });
-    });
