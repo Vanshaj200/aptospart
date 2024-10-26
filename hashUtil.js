de12b83f0da8d95c366dfbfdf720e09a73612008
Vanshaj200 <140325736+Vanshaj200@users.noreply.github.com>
2024-10-26 14:05:35 +0530

    Delete hashUtil.js

--- hashUtil.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto'); // Import crypto library
-
-// Function to generate a SHA-256 hash from input data
-function generateHash(data) {
-    return crypto.createHash('sha256').update(data).digest('hex');
-}
-
-module.exports = generateHash; // Export the function for use elsewhere
